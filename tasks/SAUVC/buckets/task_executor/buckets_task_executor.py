2b436ba21e32df273944c6649a9397cbba4f2dcd
DominikWasiolka <dominowasy@gmail.com>
2020-01-28 15:32:57 +0100

    modify gate dask executor - delete useless functions

--- tasks/SAUVC/buckets/task_executor/buckets_task_executor.py
+++ tasks/SAUVC/buckets/task_executor/buckets_task_executor.py
@@ -1,36 +1,37 @@
-from tasks.task_executor_itf import ITaskExecutor, Cameras
+from tasks.task_executor_itf import ITaskExecutor
+from neural_networks.DarknetClient import DarknetClient
 from communication.rpi_broker.movements import Movements
-from communication.rpi_broker.hydrophones import Hydrophones
-from tasks.gate.locator.ml_solution.yolo_soln import YoloBucketLocator
 from utils.stopwatch import Stopwatch
 from structures.bounding_box import BoundingBox
-from utils.signal_processing import mvg_avg
 from configs.config import get_config
 from time import sleep
+from definitions import IP_ADDRESS, DARKNET_PORT
 
 class BucketTaskExecutor(ITaskExecutor):
 
     def __init__(self, control_dict: Movements,
-                sensors_dict, cameras_dict: Cameras,
+                sensors_dict, camera_client,
                 main_logger):
         self._control = control_dict
-        self._front_camera = cameras_dict['front_cam1']
         self._bounding_box = BoundingBox(0, 0, 0, 0)
-        self._hydrophones = Hydrophones()
+        self._hydrophones = sensors_dict['hydrophones']
         self._logger = main_logger
         self.config = get_config("tasks")['buckets_task']
         self.MAX_TIME_SEC = self.config['max_time_sec']
         self.PINGER_LOOP_COUNTER = self.config['pinger_loop_conunter']
         self.BLUE_LOOP_COUNTER = self.config['blue_loop_counter']
         self.ANY_BUCKET_COUNTER = self.config['any_bucket_counter']
-        self.img_server = PythonRESTSubtask("utils/img_server.py", 6669)
-        self.img_server.start()
         self._control.pid_turn_on()
         self._control.pid_hold_depth(get_config('depth'))
+
+        self.darknet_client = DarknetClient(DARKNET_PORT, IP_ADDRESS)
+
         self._logger.log('Buckets: diving')
 
     def run(self):
         self._logger.log("Buckets task exector started")
+
+        self.darknet_client.load_model("bucket")
         
         stopwatch = Stopwatch()
         stopwatch.start()
--- tasks/SAUVC/gate/task_executor/gate_task_executor.py
+++ tasks/SAUVC/gate/task_executor/gate_task_executor.py
@@ -1,9 +1,7 @@
-from tasks.task_executor_itf import ITaskExecutor, Cameras
+from tasks.task_executor_itf import ITaskExecutor
 from communication.rpi_broker.movements import Movements
-from tasks.gate.locator.ml_solution.yolo_soln import YoloGateLocator
 from utils.stopwatch import Stopwatch
 from structures.bounding_box import BoundingBox
-from utils.signal_processing import mvg_avg
 from configs.config import get_config
 from time import sleep
 import cv2
@@ -13,14 +11,12 @@ import numpy as np
 class GateTaskExecutor(ITaskExecutor):
 
     ###Initialization###
-    def __init__(self, contorl_dict: Movements, sensors_dict, cameras_dict: Cameras, main_logger):
+    def __init__(self, contorl_dict: Movements, sensors_dict, camera_client, main_logger):
         self._control = contorl_dict
-        self._front_camera = cameras_dict['front_cam1']
+        self._front_camera = camera_client['front_cam1']
         self._bounding_box = BoundingBox(0, 0, 0, 0)
         self._logger = main_logger
         self.config = get_config("tasks")['gate_task']
-        self.img_server = PythonRESTSubtask("utils/img_server.py", 6669)
-        self.img_server.start()
         # For which path we are taking angle. For each path, rotation
         # angle might be set differently in config.json
         self.number = 0
@@ -48,17 +44,6 @@ class GateTaskExecutor(ITaskExecutor):
 
         if not self.go_trough_gate():
             self._logger.log("GateTE: going through the gate")
-
-#Przycina obraz, zostaje sam bbox
-    def post_image(self, img, bounding_box = None):
-        if bounding_box is not None:
-            self.img_server.post("set_img", img, unpickle_result=False)
-            bb = bounding_box.denormalize(img.shape[1], img.shape[0])
-            p1 = (int(bb.x1), int(bb.y1))
-            p2 = (int(bb.x2), int(bb.y2))
-            img = cv2.rectangle(img, p1, p2, (255,0,255))
-        self._logger.log("img posted")
-        self.img_server.post("set_img", img, unpickle_result=False)
 
     ###Find gate###
     def find_gate(self):
@@ -105,7 +90,6 @@ class GateTaskExecutor(ITaskExecutor):
         CONFIDENCE_THRESHOLD = get_config('confidence_threshold')
 
         bounding_box = YoloGateLocator().get_gate_bounding_box(img)
-        self.post_image(img, bounding_box)
 
         if bounding_box is not None:
             confidence = mvg_avg(1, confidence, MOVING_AVERAGE_DISCOUNT) #co robi funkcja mvg_avg
@@ -127,7 +111,6 @@ class GateTaskExecutor(ITaskExecutor):
         CONFIDENCE_THRESHOLD = get_config('confidence_threshold')
 
         bounding_box = YoloGateLocator().get_obstacle_bounding_box(img)  # Funkcja rozpoznawania przeszkody do dodania
-        self.post_image(img, bounding_box)
 
         if bounding_box is not None:
             confidence = mvg_avg(1, confidence, MOVING_AVERAGE_DISCOUNT)
@@ -285,3 +268,6 @@ class GateTaskExecutor(ITaskExecutor):
             self._control.move_distance(front=segment["distance"])
         self._logger.log("passed through the gate")
         return True
+
+    def mvg_avg(new_x, last_x, discount_factor):
+        return (1 - discount_factor) * new_x + last_x
