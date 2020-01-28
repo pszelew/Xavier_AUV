12ec369edd4327122a013d8915e246ec861ec710
jedrix-pl <jedrzej.jamnicki@gmail.com>
2020-01-28 20:23:07 +0100

    chandges in nn

--- run.sh
+++ run.sh
@@ -3,10 +3,12 @@
 pkill -9 python3
 pkill -9 python
 
+sleep 2
+
 bash -c 'find ./logs/ -name "*.log" -type f -delete'
 
-python3 -m neural_networks.DarknetServer.py -m "./neural_networks/models/gate/" &
+python3 -m neural_networks.DarknetServer -m './neural_networks/models/gate' &
 
-python3 -m camera_server.serverXavier.py &
-sleep 1 && python3 -m camera_serve.saveCameraClient &
-sleep 2 && python3 main.py &
+python3 -m camera_server.serverXavier &
+sleep 1 && python3 -m camera_server.saveCameraClient &
+sleep 5 && python3 main.py 
