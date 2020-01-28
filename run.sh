586a532caeea950362cfd1ea705e21223e97b6db
DominikWasiolka <dominowasy@gmail.com>
2020-01-28 14:18:47 +0100

    reorganize run.sh and modify saveCameraClient.py

--- run.sh
+++ run.sh
@@ -3,5 +3,8 @@
 pkill -9 python3
 pkill -9 python
 
+bash -c 'find ./logs/ -name "*.log" -type f -delete'
+
 python3 -m camera_server.serverXavier.py &
-sleep 2 && python3 main.py 
+sleep 1 && python3 -m camera_serve.saveCameraClient &
+sleep 2 && python3 main.py &
