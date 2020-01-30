d95eb8e037bf04cb8ac6e60b056aaf935a46b3c1
krzysiekdzi <krzysztofdziuba97@gmail.com>
2020-01-30 10:42:29 +0100

    Changes in centering, hydrophones. Development of buckets task algorythm.

--- communication/rpi_broker/hydrophones.py
+++ communication/rpi_broker/hydrophones.py
@@ -6,5 +6,8 @@ class Hydrophones:
     def __init__(self, rpi_reference):
         self.rpi_reference = rpi_reference
 
-    def get_angle(self):
-        return self.rpi_reference.get_angle()
+    def get_angle(self,frequency):
+        '''
+        @param frequency is a listening frequency for demanded pinger
+        '''
+        return self.rpi_reference.get_angle(frequency)
