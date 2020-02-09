63447fa635f18fa04bf74e55c569dc67209ed5a1
krzysiekdzi <krzysztofdziuba97@gmail.com>
2020-02-09 16:12:02 +0100

    Changes in centering (only proportional).

--- utils/centering.py
+++ utils/centering.py
@@ -1,5 +1,13 @@
+from time import sleep
+from configs.config import get_config
 def center_rov(move, xPos = 0, yPos = 0, Bbox = None, depth_sensor = None):
-    Kp = 0.01
+    config = get_config("tasks")['path_task']
+    Kp = config['centering']['kp']
+    """
+    TODO implement saving previous position outside center_rov()
+    it is needed for integral and derivational part of regulator
+    """
+    '''
     Ki = 0.1
     Kd = 0.1
     previousFlarePosX = 0
@@ -8,10 +16,12 @@ def center_rov(move, xPos = 0, yPos = 0, Bbox = None, depth_sensor = None):
     errorY = 0
     errorSumX = 0
     errorSumY = 0
+    '''
     if Bbox:
         xPos = Bbox.x
         yPos = Bbox.y
         
+    '''
     if previousFlarePosX != 0 & previousFlarePosY != 0:
         errorX = xPos - previousFlarePosX
         errorY = yPos - previousFlarePosY
@@ -20,8 +30,8 @@ def center_rov(move, xPos = 0, yPos = 0, Bbox = None, depth_sensor = None):
 
     previousFlarePosX = xPos
     previousFlarePosY = yPos
-
-    move.set_lin_velocity(right = xPos * Kp + Ki * errorSumX + Kd * errorX)
+'''
+    move.set_lin_velocity(right = xPos * Kp )#+ Ki * errorSumX + Kd * errorX)
     if depth_sensor:
         current_depth = depth_sensor.get_depth()
         move.pid_set_depth(current_depth - Kp * yPos)
